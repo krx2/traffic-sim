04f41485dc95c1c0bd47b3b2b7e6b9ff410ff733
Konrad Zebro <konrad.zebro@gmail.com>
2026-04-30 14:20:45 +0200

    Initial commit

---
+++ src/main/java/com/kzebro/trafficsim/TrafficSimApplication.java
@@ -0,0 +1,13 @@
+package com.kzebro.trafficsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficSimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kzebro/trafficsim/TrafficSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzebro.trafficsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
